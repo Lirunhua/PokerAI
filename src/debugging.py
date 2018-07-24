2b566380e9ed8507f3bba900bb3848329ffd7915
davidb <David_Bonsant@trendmicro.com>
2018-07-24 14:02:03 -0400

    Improved debug.py

--- src/debugging.py
+++ src/debugging.py
@@ -2,15 +2,10 @@ from main import Main
 import threading
 
 if __name__ == '__main__':
-    m1 = Main('GLaDOS')
-    t1 = threading.Thread(target=m1.doListen)
-    t1.start()
-    m2 = Main('GLaDOS2')
-    t2 = threading.Thread(target=m2.doListen)
-    t2.start()
-    m3 = Main('GLaDOS3')
-    t3 = threading.Thread(target=m3.doListen)
-    t3.start()
-    t1.join()
-    t2.join()
-    t3.join()
+	thread = [threading.Thread(target=Main('GLaDOS%d'%i).doListen) for i in range(20)]
+
+	for t in thread:
+		t.start()
+
+	for t in thread:
+		t.join()
