50b56dd918690db7cf1d80b4cd8fa3758b6f7441
Jess Cannarozzo <Jesscannarozzo@cmail.carleton.ca>
2018-07-30 14:41:07 -0400

    fix indentation in debugging.py, works now

--- src/debugging.py
+++ src/debugging.py
@@ -2,14 +2,14 @@ from main import Main
 import threading
 
 if __name__ == '__main__':
-  main = [Main('GLaDOS%d'%i) for i in range(10)]
-  thread = []
-  for m in main:
-    m.debugMode = True
-	  thread.append(threading.Thread(target=m.doListen))
+    main = [Main('GLaDOS%d' % i) for i in range(10)]
+    thread = []
+    for m in main:
+        m.debugMode = True
+        thread.append(threading.Thread(target=m.doListen))
 
-	for t in thread:
-		t.start()
+    for t in thread:
+        t.start()
 
-	for t in thread:
-		t.join()
+    for t in thread:
+        t.join()
